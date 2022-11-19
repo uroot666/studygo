681b676cffa8074e40a28cb1ccd5905bad377513
uroot666 <uroot666@gmail.com>
2022-11-20 00:29:13 +0800

    gin 数据解析

--- day13/go_5/main.go
+++ day13/go_5/main.go
@@ -4,9 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+// 定义接收数据的结构体
+type Login struct {
+	// binding:"required" 修饰的字段，若接收为空值，则报错，是必须字段
+	User     string `form:"username" json:"user" uri:"user" xml:"user" binding:"required"`
+	Password string `form:"password" json:"password" uri:"password" xml:"password" binding:"required"`
+}
 
 func main() {
 	// 1. 创建路由
@@ -44,10 +52,71 @@ func main() {
 		c.SaveUploadedFile(file, file.Filename)
 		c.String(http.StatusOK, fmt.Sprintf("%s upload", file.Filename))
 	})
+
+	r.POST("loginJSON", func(c *gin.Context) {
+		// 声明接收的变量
+		var json1 Login
+		// 将request的body中的数据，自动按照json格式解析到结构体
+		if err := c.ShouldBindJSON(&json1); err != nil {
+			// 返回错误信息
+			// gin.H 封装了生成json数据的工具
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		// 判断用户名密码是否正确
+		if json1.User != "root" || json1.Password != "admin" {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "304"})
+		}
+		c.JSON(http.StatusOK, gin.H{"status": "200"})
+
+	})
+
+	v1 := r.Group("/v1")
+	// {} 是书写规范
+	{
+		v1.GET("/login", login)
+		v1.GET("/submit", submit)
+	}
+
+	v2 := r.Group("/v2")
+	{
+		v2.POST("/login", login)
+		v2.POST("/submit", submit)
+	}
+
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/index", func(c *gin.Context) {
+		// 根据文件名渲染
+		// 最终json将title替换
+		c.HTML(200, "index.tmpl", gin.H{"title": "我的标题"})
+	})
+
+	r.GET("/long_async", func(c *gin.Context) {
+		// 需要搞一个副本
+		copyContext := c.Copy()
+		// 异步处理
+		go func() {
+			time.Sleep(3 * time.Second)
+			log.Println("异步执行: " + copyContext.Request.RequestURI)
+		}()
+	})
+
+	// 同步
+	r.GET("long_sync", func(c *gin.Context) {
+		time.Sleep(3 * time.Second)
+		log.Println("同步执行" + c.Request.RequestURI)
+	})
+
 	r.Run(":80")
 
 }
 
-func getting(c *gin.Context) {
+func login(c *gin.Context) {
+	name := c.DefaultQuery("name", "jack")
+	c.String(200, fmt.Sprintf("hello %s\n", name))
+}
 
+func submit(c *gin.Context) {
+	name := c.DefaultQuery("name", "jack")
+	c.String(200, fmt.Sprintf("hello %s\n", name))
 }
